aef496194a7f64bea4d683361503a9b30d32f6bd
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2024-08-13 10:01:58 +0530

    vulpes-majora(rules.mk): add feature flags for compiling firmware

--- keyboards/fingerpunch/vulpes_majora/v1/keymaps/taz/rules.mk
+++ keyboards/fingerpunch/vulpes_majora/v1/keymaps/taz/rules.mk
@@ -2,3 +2,8 @@ MACCEL_ENABLE ?= no
 ifeq ($(strip $(MACCEL_ENABLE)), yes)
 	include $(USER_PATH)/features/maccel/maccel/rules.mk
 endif
+
+AUDIO_ENABLE = yes
+ENCODER_ENABLE = yes
+FP_TRACKBALL_ENABLE = yes
+RGBLIGHT_ENABLE = yes
